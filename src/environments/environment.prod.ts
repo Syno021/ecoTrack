4a3d6ba84fcc7baaceb541d056e96ec7aa38f48d
Syno021 <synoivfour@gmail.com>
2025-04-13 16:57:22 +0200

    new global css

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCbmGiV-kV7ZU4-MZUseWRvKoeBePseGTY",
+    authDomain: "ecotrack-ab0d2.firebaseapp.com",
+    projectId: "ecotrack-ab0d2",
+    storageBucket: "ecotrack-ab0d2.firebasestorage.app",
+    messagingSenderId: "358581396901",
+    appId: "1:358581396901:web:821e24b4378ba40fe5344c",
+    measurementId: "G-JPYMV3925Y"
+    }
 };
